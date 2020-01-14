6bc35888443bb7e80ee17d716afdbeb12a229328
Ha Nguyen <hnguyen68@academic.rrc.ca>
2020-01-14 16:58:43 -0600

    fixed react-scripts not found bugs for client side

--- src/utils/API/API.js
+++ src/utils/API/API.js
@@ -4,11 +4,11 @@ let authToken = "";
 
 if (localStorage.getItem("token")) {
     authToken = localStorage.getItem("token");
-   
+
 }
 
 export default axios.create({
-    baseURL: "http://10.0.0.51:3000/",
+    baseURL: "http://localhost:3000",
     headers: { common: { Authorization: `Bearer ${authToken}` } }
 });
 
