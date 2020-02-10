a481cb6b50fe8c289cf044781606401bc26c15b9
Ha Nguyen <hnguyen68@academic.rrc.ca>
2020-02-10 11:55:56 -0600

    Display accounts with collapsible tab

--- src/features/projects/ProjectAccounts.jsx
+++ src/features/projects/ProjectAccounts.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import {Row} from "react-materialize"
-import AccountForm from "../account/AccountForm"
+import { Row, Collapsible, CollapsibleItem, Icon } from "react-materialize";
+import AccountForm from "../account/AccountForm";
 import CreateAccountModal from "../account/CreateAccountModal";
 
 function mapStateToProps(state) {
@@ -9,15 +9,30 @@ function mapStateToProps(state) {
 }
 
 class ProjectAccounts extends Component {
-
   render() {
-    const {accounts} = this.props
+    const { accounts } = this.props;
     return (
       <div>
-         <CreateAccountModal projectId={this.props.projectId} getProjectDetail={this.props.getProjectDetail}/>
+        <CreateAccountModal
+          projectId={this.props.projectId}
+          getProjectDetail={this.props.getProjectDetail}
+        />
+        {accounts.length > 0 &&
+        <Collapsible accordion>
           {accounts.map(account => {
-            return <AccountForm account={account} key={account._id}/>
+            return (
+              <CollapsibleItem
+                expanded={false}
+                header={account.domain}
+                icon={<Icon>email</Icon>}
+                node="div"
+                key={account._id}
+              >
+                <AccountForm account={account}/>
+              </CollapsibleItem>
+            );
           })}
+        </Collapsible>}
       </div>
     );
   }
