45904c08904fb636513b99790820bca04e7dc3c9
Ha Nguyen <hnguyen68@academic.rrc.ca>
2020-02-12 10:20:41 -0600

    Fixed bugs for deployment

--- src/utils/API/API.js
+++ src/utils/API/API.js
@@ -10,7 +10,7 @@ if (localStorage.getItem("token")) {
 export default axios.create({
   // baseURL: "https://beeblee-crm-node.herokuapp.com",
 
-  baseURL: "https://beeblee-example-node.herokuapp.com/",
+  baseURL: "https://beeblee-example-node.herokuapp.com",
   //baseURL: "http://localhost:3000",
   headers: { common: { Authorization: `Bearer ${authToken}` } }
 });
