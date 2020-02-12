b83d01f82ebbc90d46d57a681cb978eb9fa2405a
Ha Nguyen <hnguyen68@academic.rrc.ca>
2020-02-12 11:24:00 -0600

    Upload All Nodes Module on Git

--- src/utils/API/API.js
+++ src/utils/API/API.js
@@ -9,7 +9,6 @@ if (localStorage.getItem("token")) {
 
 export default axios.create({
   // baseURL: "https://beeblee-crm-node.herokuapp.com",
-
   baseURL: "https://beeblee-example-node.herokuapp.com",
   //baseURL: "http://localhost:3000",
   headers: { common: { Authorization: `Bearer ${authToken}` } }
